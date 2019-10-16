f14a650b7941b72adde06b1f0d4a3edb27885662
Shengyou Fan <shengyoufan@gmail.com>
2019-10-16 13:53:43 +0800

    install exposed and mysql-connector

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val exposed_version: String by project
+val mysql_connector_version:String by project
 
 plugins {
     application
@@ -26,6 +28,8 @@ dependencies {
     testCompile("io.ktor:ktor-server-tests:$ktor_version")
     implementation("io.ktor:ktor-html-builder:$ktor_version")
     implementation("io.ktor:ktor-freemarker:$ktor_version")
+    implementation("org.jetbrains.exposed:exposed:$exposed_version")
+    implementation("mysql:mysql-connector-java:$mysql_connector_version")
 }
 
 kotlin.sourceSets["main"].kotlin.srcDirs("src")
